667df9e4fc92b562d2853171ceef7e8c506d1e17
Efezino Idisi <enduranceidisi13@gmail.com>
2024-03-29 20:06:31 +0100

    complete day 6

---
+++ all-projects/06-type-ahead/script.js
@@ -0,0 +1,62 @@
+"use strict";
+const form = document.querySelector(".search-form");
+const query = document.querySelector(".search");
+
+const suggestionsElement = document.querySelector(".suggestions");
+
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const store = [];
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then((data) => store.push(...data))
+  .catch((err) => {
+    console.log(err);
+  });
+
+/**
+ *
+ * @param {number} x
+ * @returns string
+ */
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function displayResults(value) {
+  const results = store.filter(
+    (item) =>
+      item.city.toLowerCase().includes(value) ||
+      item.state.toLowerCase().includes(value)
+  );
+
+  const html = results.map((result) => {
+    const regex = new RegExp(value, "gi");
+    const city = result.city.replace(regex, `<span class="hl">${value}</span>`);
+    const state = result.state.replace(
+      regex,
+      `<span class="hl">${value}</span>`
+    );
+
+    return `<li>
+    <span>${city}, ${state}</span>
+    <span>${numberWithCommas(result.population)}</span>
+    </li>`;
+  });
+
+  suggestionsElement.innerHTML = html.join("");
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  const value = query.value.toLowerCase();
+
+  displayResults(value);
+}
+
+form.addEventListener("submit", handleFormSubmit);
+query.addEventListener("keyup", (e) =>
+  displayResults(e.target.value.toLowerCase())
+);
